613ef0104427b578aced9250230ebd3761567973
Steropor <steroporstop@gmail.com>
2023-12-27 18:53:03 +0100

    main: Demo

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,9 @@ import TopHeader from "../components/_App/TopHeader";
 import Navbar from "../components/_App/Navbar";
 import HeroSlider from "../components/Home/HeroSlider";
 import AboutUs from "../components/Home/AboutUs";
-import OurExpertise from "../components/Home/OurExpertise";
 import Services from "../components/Home/Services";
-import HowCovid19Spreads from "../components/Home/HowCovid19Spreads";
-import SymptomsCovid19 from "../components/Home/SymptomsCovid19";
-import FaqSection from "../components/Common/FaqSection";
-import VideoIntro from "../components/Common/VideoIntro";
 import OurDoctors from "../components/Common/OurDoctors";
-import Stats from "../components/Home/Stats";
 import FeedbackSlider from "../components/Home/FeedbackSlider";
-import LatestBlogPost from "../components/Common/LatestBlogPost";
-import NewsletterForm from "../components/Common/NewsletterForm";
 import Footer from "../components/_App/Footer";
 
 const Index = () => {
@@ -25,30 +17,11 @@ const Index = () => {
 
       <AboutUs />
 
-      {/* <OurExpertise /> */}
-
       <Services />
 
       <FeedbackSlider />
 
-      {/* <HowCovid19Spreads /> */}
-
-      {/* <SymptomsCovid19 /> */}
-
-      {/* <FaqSection /> */}
-
-      {/* <VideoIntro /> */}
-
       <OurDoctors />
-
-      {/* <div className="pb-100">
-        <Stats />
-      </div> */}
-
-
-      {/* <LatestBlogPost /> */}
-
-      {/* <NewsletterForm /> */}
 
       <Footer />
     </>
